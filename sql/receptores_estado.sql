5a7e7f0482fc1d31891bd3bbe88a37a146101d4d
haze123 <rarp321@gmail.com>
2026-09-14 19:57:12 -0500

    chore: quitar script sql

--- sql/receptores_estado.sql
+++
@@ -1,4 +0,0 @@
--- Eliminado logico de clientes: ejecutar una vez en la BD auxiliar del grifo antes de desplegar.
--- Los clientes existentes quedan activos (estado = 1).
-IF COL_LENGTH('Receptores', 'estado') IS NULL
-    ALTER TABLE Receptores ADD estado bit NOT NULL CONSTRAINT DF_Receptores_estado DEFAULT 1;
